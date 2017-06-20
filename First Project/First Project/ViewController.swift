dac6d255142c2e9a99db7c294b7043a9403bef68
uscitp-jeantu <tujean27@gmail.com>
2017-06-19 22:27:34 -0700

    learning how to connect different buttons and labels together while using a text field

--- First Project/First Project/ViewController.swift
+++ First Project/First Project/ViewController.swift
@@ -10,13 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var myLabel: UILabel!
-
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var secondLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        secondLabel.text = "" // making it empty
     }
     @IBAction func didTapButton(_ sender: Any) { //for the "Tap Me!" button 
         myLabel.text = "Changed with Code!"
+        
+    }
+    @IBAction func didTapBotton(_ sender: Any) {
+        let name = textField.text! //there may not be any data in the field, so we add the exclamation mark, to unwrapp the value 
+        secondLabel.text = "Hi \(name)!"
 
     }
 
