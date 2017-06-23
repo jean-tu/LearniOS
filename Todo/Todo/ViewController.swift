f2d5193efdddcb78a71f109b1dfd676e3ebfe31c
uscitp-jeantu <tujean27@gmail.com>
2017-06-22 21:18:15 -0700

    made a simple todo list

--- Todo/Todo/ViewController.swift
+++ Todo/Todo/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var txtInput: UITextField!
+    @IBOutlet weak var txtOutput: UITextView!
+    
+    var items: [String] = [] //array of strings to hold the items
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func addItem(_ sender: Any) {
+        if(txtInput.text == ""){//checking to make sure that there's text inside the textfield
+            return
+        }
+        items.append(txtInput.text!)
+        txtOutput.text = "" // initiallizing it to nothing
+        for item in items {
+            txtOutput.text.append("\(item)\n")
+        }
+        txtInput.text = 	"" // clearing the input field
     }
 
     override func didReceiveMemoryWarning() {
